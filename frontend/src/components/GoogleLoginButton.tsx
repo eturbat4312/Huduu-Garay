e3a67559c10a8fb220a1bfc21e0a8922f75c83bc
tory4312 <86555871+tory4312@users.noreply.github.com>
2025-08-26 23:15:46 +0200

    google commit

--- frontend/src/components/GoogleLoginButton.tsx
+++ frontend/src/components/GoogleLoginButton.tsx
@@ -25,8 +25,7 @@ export default function GoogleLoginButton() {
       client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
       callback: async (response: GoogleCredentialResponse) => {
         try {
-          console.log("🔎 Will POST to: /auth/google/");
-          const res = await axios.post("/auth/google/", {
+          const res = await axios.post("/api/auth/google/", {
             access_token: response.credential,
           });
 
